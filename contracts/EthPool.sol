84d67e48e10f9bd95f4f564aa7744d09c74a9677
kifen <kifenraph@gmail.com>
2022-03-24 15:18:25 +0200

    Update accounts storage

--- contracts/EthPool.sol
+++ contracts/EthPool.sol
@@ -5,19 +5,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EthPool is Ownable {
     // struct of deposit with corresponding reward.
-    struct Deposit {
-        uint256 deposit;
-        uint256 reward;
+    struct Account {
+        uint256 deposits;
+        uint256 rewards;
+        bool active;
     }
 
-    //  mapping of id to `Deposit`
-    mapping(uint256 => Deposit) private s_deposits;
+    uint256 public accounstCount = 0;
+    uint256 public poolValue;
 
-    // mapping of account to deposit count
-    mapping(address => uint256) private s_depositCount;
+    //  mapping of address to `Account`
+    //mapping(address => Account) private s_accounts;
+    mapping(address => uint256) private s_indexOf;
+
+    // array of all accounts
+    Account[] private s_accounts;
 
     event DepositETH(address account, uint256 amount);
-    event DepositReward(address account, uint256 amount);
+    event DepositReward(uint256 amount, uint256 accounts);
     event Withdraw(address account, uint256 deposits, uint256 rewards);
 
     /**
@@ -26,14 +31,43 @@ contract EthPool is Ownable {
     function depositEth() external payable {
         require(msg.value > 0, "EthPool: no value sent");
 
-        uint256 depositCount = s_depositCount[msg.sender];
-        depositCount += 1;
+        uint256 accountIndex = s_indexOf[msg.sender];
+        Account memory account = s_accounts[accountIndex];
 
-        Deposit memory newDeposit = Deposit(msg.value, 0);
+        if (_isNewAccount(accountIndex, account.active)) {
+            account = Account(msg.value, 0, true);
+            s_accounts.push(account);
+            s_indexOf[msg.sender] = accounstCount;
+            accounstCount++;
+        } else {
+            uint256 totalDeposits = account.deposits + msg.value;
+            account.deposits = totalDeposits;
+            s_accounts[accountIndex] = account;
+        }
 
-        s_deposits[depositCount] = newDeposit;
+        poolValue += msg.value;
 
         emit DepositETH(msg.sender, msg.value);
+    }
+
+    function depositReward(uint256 _rewards) external onlyOwner {
+        Account[] memory accounts = s_accounts;
+
+        Account memory account;
+        uint256 numberAccounts;
+        for (uint256 i = 0; i < accounts.length; i++) {
+            account = accounts[i];
+            uint256 accountReward = _getAccountReward(
+                _rewards,
+                account.deposits
+            );
+
+            account.rewards = account.rewards + accountReward;
+            s_accounts[i] = account;
+            numberAccounts += 1;
+        }
+
+        emit DepositReward(_rewards, numberAccounts);
     }
 
     /**
@@ -46,6 +80,15 @@ contract EthPool is Ownable {
             uint256 totalRewards
         ) = _getTotalDepositsAndRewards(msg.sender);
 
+        uint256 accountIndex = s_indexOf[msg.sender];
+        Account memory account = s_accounts[accountIndex];
+
+        account.deposits = account.deposits - totalDeposits;
+        account.deposits = account.rewards - totalRewards;
+        s_accounts[accountIndex] = account;
+
+        poolValue -= totalDeposits;
+
         uint256 withdrawAmount = totalDeposits + totalRewards;
         (bool sent, ) = payable(msg.sender).call{value: withdrawAmount}("");
 
@@ -57,7 +100,19 @@ contract EthPool is Ownable {
      * @notice check if an account has deposits
      */
     function _hasDeposits(address _account) public view returns (bool) {
-        if (s_depositCount[_account] > 0) return true;
+        uint256 accountIndex = s_indexOf[_account];
+
+        if (s_accounts[accountIndex].deposits > 0) return true;
+        return false;
+    }
+
+    /**
+     * @notice check if an account has rewards
+     */
+    function _hasRewards(address _account) public view returns (bool) {
+        uint256 accountIndex = s_indexOf[_account];
+
+        if (s_accounts[accountIndex].rewards > 0) return true;
         return false;
     }
 
@@ -69,16 +124,28 @@ contract EthPool is Ownable {
         view
         returns (uint256 totalDeposits, uint256 totalRewards)
     {
-        uint256 depositCount = s_depositCount[_account];
+        uint256 accountIndex = s_indexOf[_account];
+        Account memory account = s_accounts[accountIndex];
+        totalDeposits = account.deposits;
+        totalRewards = account.rewards;
+    }
 
-        uint256 limit = depositCount + 1;
-        uint256 j = 1;
+    function _getAccountReward(uint256 _reward, uint256 _totalDeposits)
+        internal
+        returns (uint256)
+    {
+        uint256 rewardPercentage = (_totalDeposits * 100) / poolValue;
 
-        while (j < limit) {
-            Deposit memory deposit = s_deposits[j];
-            totalDeposits += deposit.deposit;
-            totalRewards += deposit.reward;
-            j++;
-        }
+        return (rewardPercentage * _reward) / 100;
+    }
+
+    function _isNewAccount(uint256 _accountIndex, bool _active)
+        internal
+        returns (bool)
+    {
+        if (_accountIndex == 0 && s_accounts.length == 0) return true;
+        else if (_accountIndex == 0 && !_active) return true;
+
+        return false;
     }
 }
